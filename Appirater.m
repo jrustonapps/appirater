f552f702b29b71e1a15ff96bbe71c0b7173483ff
Jake Ruston <jake@jrustonapps.com>
2017-08-14 10:57:28 +0200

    Affiliate code iOS 10.3 reviews

--- Appirater.m
+++ Appirater.m
@@ -35,6 +35,7 @@
  */
 
 #import <SystemConfiguration/SystemConfiguration.h>
+#import <StoreKit/StoreKit.h>
 #import <CFNetwork/CFNetwork.h>
 #import "Appirater.h"
 #include <netinet/in.h>
@@ -53,7 +54,7 @@ NSString *const kAppiraterReminderRequestDate		= @"kAppiraterReminderRequestDate
 
 NSString *templateReviewURL = @"itms-apps://ax.itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=APP_ID";
 NSString *templateReviewURLiOS7 = @"itms-apps://itunes.apple.com/app/idAPP_ID";
-NSString *templateReviewURLiOS8 = @"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software";
+NSString *templateReviewURLiOS8 = @"itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?id=APP_ID&onlyLatestVersion=true&pageNumber=0&sortOrdering=1&type=Purple+Software&action=write-review";
 
 static NSString *_appId;
 static double _daysUntilPrompt = 30;
@@ -70,6 +71,8 @@ static BOOL _usesAnimation = TRUE;
 static UIStatusBarStyle _statusBarStyle;
 static BOOL _modalOpen = false;
 static BOOL _alwaysUseMainBundle = NO;
+static NSString *_affiliateToken = nil;
+static NSString *_affiliateCampaign = nil;
 
 @interface Appirater ()
 @property (nonatomic, copy) NSString *alertTitle;
@@ -159,6 +162,14 @@ static BOOL _alwaysUseMainBundle = NO;
 }
 + (void)setAlwaysUseMainBundle:(BOOL)alwaysUseMainBundle {
     _alwaysUseMainBundle = alwaysUseMainBundle;
+}
+
++ (void)setAffiliateToken:(NSString*)token {
+    _affiliateToken = token;
+}
+
++ (void)setAffiliateCampaign:(NSString*)campaign{
+    _affiliateCampaign = campaign;
 }
 
 + (NSBundle *)bundle
@@ -634,10 +645,23 @@ static BOOL _alwaysUseMainBundle = NO;
 
 	//Use the in-app StoreKit view if available (iOS 6) and imported. This works in the simulator.
 	if (![Appirater sharedInstance].openInAppStore && NSStringFromClass([SKStoreProductViewController class]) != nil) {
+        
+        if (NSStringFromClass([SKStoreReviewController class]) != nil) {
+            [SKStoreReviewController requestReview];
+            return;
+        }
 		
 		SKStoreProductViewController *storeViewController = [[SKStoreProductViewController alloc] init];
 		NSNumber *appId = [NSNumber numberWithInteger:_appId.integerValue];
-		[storeViewController loadProductWithParameters:@{SKStoreProductParameterITunesItemIdentifier:appId} completionBlock:nil];
+		NSMutableDictionary* params = [NSMutableDictionary dictionaryWithObject:appId forKey:SKStoreProductParameterITunesItemIdentifier];
+        
+        if (_affiliateToken != nil) {
+            [params setValue:_affiliateToken forKey:SKStoreProductParameterAffiliateToken];
+        }
+        if (_affiliateCampaign != nil) {
+           [params setValue:_affiliateCampaign forKey:SKStoreProductParameterCampaignToken];
+        }
+		[storeViewController loadProductWithParameters:params completionBlock:nil];
 		storeViewController.delegate = self.sharedInstance;
         
         id <AppiraterDelegate> delegate = self.sharedInstance.delegate;
@@ -650,6 +674,11 @@ static BOOL _alwaysUseMainBundle = NO;
 	
 	//Use the standard openUrl method if StoreKit is unavailable.
 	} else {
+        
+        if (NSStringFromClass([SKStoreReviewController class]) != nil) {
+            [SKStoreReviewController requestReview];
+            return;
+        }
 		
 		#if TARGET_IPHONE_SIMULATOR
 		NSLog(@"APPIRATER NOTE: iTunes App Store is not supported on the iOS simulator. Unable to open App Store page.");
@@ -665,6 +694,13 @@ static BOOL _alwaysUseMainBundle = NO;
         else if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
         {
             reviewURL = [templateReviewURLiOS8 stringByReplacingOccurrencesOfString:@"APP_ID" withString:[NSString stringWithFormat:@"%@", _appId]];
+        }
+        
+        if (_affiliateToken) {
+            reviewURL = [reviewURL stringByAppendingFormat:@"&at=%@", _affiliateToken];
+        }
+        if (_affiliateCampaign) {
+            reviewURL = [reviewURL stringByAppendingFormat:@"&ct=%@", _affiliateCampaign];
         }
 
 		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:reviewURL]];
